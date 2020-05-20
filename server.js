737739e7b05f2bd86025355e5f938e435e26b637
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-05-20 11:16:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var data = require('./data/test.json');
 var app = express();
 const PORT = 3000;
 
@@ -9,4 +10,5 @@ app.use(express.static(__dirname));
 
 app.listen(PORT,()=>{
   console.log(`Hey Budd! App is running on port ${PORT}`);
+  console.log(data);
 });
