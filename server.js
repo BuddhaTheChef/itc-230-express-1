2b17706c88d1e1445503c357df72d7454ea8ecf8
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-05-20 10:59:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+const PORT = 3000;
 
 //this allows you to serve static pages
 app.use(express.static(__dirname));
 
-app.listen(3000);
+//app.listen(3000);
+
+app.listen(PORT,()=>{
+  console.log(`Hey Budd! App is running on port ${PORT}`);
+});
