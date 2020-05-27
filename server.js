84f5c2d02671c0ccb1618c17385a495ca1b91fb4
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-05-27 10:55:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var data = require('./data/test.json');
 var app = express();
 const PORT = 3000;
 
-//this allows you to serve static pages
-// app.use(express.static(__dirname));
+app.set('view engine', 'ejs'); //set views to ejs 
 
 //this is how a route is built in express
 app.get('/',(req,res)=>{
-  res.send(`Hey Budd! App is running on port ${PORT}`);
-  res.json(data);
+  //res.send(`Hey Budd! App is running on port ${PORT}`);
+  //res.json(data);
+  res.render('pages/index');
 });
 
 //app.listen(3000);
